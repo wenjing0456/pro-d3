c405b51e5baf4701f5af9a31f37a6e23d6800d22
wenjing <wenjing@yxqiche.com>
2019-10-10 18:27:55 +0800

    add：d3

--- src/components/nodes-graph/nodes-graph.js
+++ src/components/nodes-graph/nodes-graph.js
@@ -25,16 +25,84 @@ export default {
             //         return "I’m number " + d + "!";
             //     });
 
-            var p = d3.select("body")
-                .selectAll("p")
-                .data([4, 8, 15, 16, 12,22,42,111])
-                .text(function (d) {
-                    return d + '-';
-                });
-            p.exit().append("p")
+            // var p = d3.select("body")
+            //     .selectAll("p")
+            //     .data([4, 8, 15, 16, 12,22,42,111])
+            //     .text(function (d) {
+            //         return d + '-';
+            //     });
+            // p.exit().append("p")
+            //     .text(function (d) {
+            //         return d;
+            //     });
+
+            // d3.select("body").transition()
+            //     .duration(2000)
+            //     .style("background-color", "black");
+
+            // d3.selectAll("p").style("color", function() {
+            //     return "hsl(" + Math.random() * 360 + ",100%,50%)";
+            //   });
+
+            // d3.selectAll("p").style("color", function(d, i) {
+            //     return i % 2 ? "pink" : "#eee";
+            //   });
+
+            // var body = d3.select("body");
+            // var div = body.append("div");
+            // div.html("Hello, world!");
+
+            let data = [30, 20, 39, 20, 39, 20]
+            // d3.select(".chart")
+            //     .selectAll("div")
+            //     .data(data)
+            //     .enter().append("div")
+            //     .style("width", function (d) {
+            //         return d * 10 + "px";
+            //     })
+            //     .text(function (d) {
+            //         return d;
+            //     });
+
+            // var x = d3.scaleLinear()
+            //     .domain([0, d3.max(data)])
+            //     .range([0, 420]);
+            // d3.select(".chart")
+            //     .selectAll("div")
+            //     .data(data)
+            //     .enter().append("div")
+            //     .style("width", function (d) {
+            //         return x(d) + "px";
+            //     })
+            //     .text(function (d) {
+            //         return d + '%';
+            //     });
+            var width = 420,
+                barHeight = 20;
+            var x = d3.scaleLinear()
+                .domain([0, d3.max(data)])
+                .range([0, width])
+            var chart = d3.select('.chartSvg')
+                .attr('width', width)
+                .attr('height', barHeight * data.length)
+            var bar = chart.selectAll('g')
+                .data(data)
+                .enter().append('g')
+                .attr('transform', function (d, i) {
+                    return 'translate(0,' + i * barHeight + ')';
+                })
+            bar.append('rect')
+                .attr('width', x)
+                .attr('height', barHeight - 1)
+            bar.append('text')
+                .attr('x', function (d) {
+                    return x(d) - 3;
+                })
+                .attr('y', barHeight / 2)
+                .attr('dy', '0.35em')
                 .text(function (d) {
                     return d;
-                });
+                })
         }
     },
     mounted() {
--- src/components/nodes-graph/nodes-graph.vue
+++ src/components/nodes-graph/nodes-graph.vue
@@ -7,10 +7,51 @@
         <p>234</p>
         <p>1234</p>
         <p>234</p>
+        <div class="chart">
+
+        </div>
+        <svg class="chartSvg"></svg>
+        <div class="test">
+            test
+        </div>
     </div>
 </template>
 
 <script src="./nodes-graph.js"></script>
-<style scoped>
+<style>
+    .hello p {
+        color: red;
+    }
 
+    .chartSvg rect {
+        fill: steelblue;
+    }
+
+    .chartSvg text {
+        fill: white;
+        font: 10px sans-serif;
+        text-anchor: end;
+    }
+    .test{
+        width: 100px;
+        height: 20px;
+        background: pink;
+        transform: translate(10px,100px)
+    }
+
+    /* .chart div {
+        margin-top: 10px;
+        background: bisque;
+        text-align: right;
+    }
+
+    .chart rect {
+        fill: steelblue;
+    }
+
+    .chart text {
+        fill: white;
+        font: 10px sans-serif;
+        text-anchor: end;
+    } */
 </style>
